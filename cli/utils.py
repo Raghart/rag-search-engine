37bb967d3da7971f9f1fb9e7905e7db6e1148104
Raghart <rafaelrojas36@outlook.com>
2026-02-02 20:14:55 -0400

    r filtering out stopword list

--- cli/utils.py
+++ cli/utils.py
@@ -2,11 +2,20 @@ import json, os, string
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
+STOPWORDS_PATH = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
+
 
 def load_movies():
     with open(DATA_PATH, "r") as f:
         data = json.load(f)
     return data["movies"]
+
+def load_stopwords():
+    with open(STOPWORDS_PATH, "r") as f:
+        stop_words = f.read().splitlines()
+    return stop_words
+
+STOPWORD_LIST = load_stopwords()
 
 def search_movies(data, query):
     result = []
@@ -14,17 +23,25 @@ def search_movies(data, query):
 
     for movie in data:
         tokenized_title = tokenize_text(movie["title"])
-        for query_word in tokenized_query:
-            if query_word in tokenized_title:
-                result.append(movie)
-                
+        if has_matching_token(tokenized_query, tokenized_title):
+            result.append(movie)
+            
+
     result.sort(key=lambda x: x["id"])
     return result[:5]
+
+def has_matching_token(query_tokens, movie_tokens):
+    for movie_word in movie_tokens:
+        for query in query_tokens:
+            if query in movie_word:
+                return True
+    return False
 
 def tokenize_text(title: str):
     parsed_movie = parse_movie_title(title)
     movie_tokens = parsed_movie.split()
-    return list(filter(lambda title: len(title) > 0, movie_tokens))
+
+    return list(filter(lambda word: word and word not in STOPWORD_LIST, movie_tokens))
 
 def parse_movie_title(title):
     return title.lower().translate(str.maketrans("", "", string.punctuation))
