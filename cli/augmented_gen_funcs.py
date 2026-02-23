5f9aa2034eaebff1784464761fc1b3cbff3dc542
Rafael <rafaelrojas36@outlook.com>
2026-02-23 15:27:30 -0400

    r implemmented summarize query results cmd

--- cli/augmented_gen_funcs.py
+++ cli/augmented_gen_funcs.py
@@ -20,3 +20,25 @@ def rag_search(query: str):
         contents=prompt
     )
     return movie_results, response.text
+
+def sum_search_query(query: str, limit: int):
+    search_results = rrf_search_query(query, 60, limit, None, None)
+    api_key = os.environ.get("rag-gemini-key")
+    client = genai.Client(api_key=api_key)
+    prompt = f"""
+        Provide information useful to this query by synthesizing information from multiple search results in detail.
+        The goal is to provide comprehensive information so that users know what their options are.
+        Your response should be information-dense and concise, with several key pieces of information about the genre, plot, etc. of each movie.
+        This should be tailored to Hoopla users. Hoopla is a movie streaming service.
+        Query: {query}
+        Search Results:
+        {search_results}
+        Provide a comprehensive 3–4 sentence answer that combines information from multiple sources:
+        """
+    
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents=prompt
+    )
+
+    return search_results, response.text
--- cli/augmented_generation_cli.py
+++ cli/augmented_generation_cli.py
@@ -1,5 +1,5 @@
 import argparse
-from augmented_gen_funcs import rag_search
+from augmented_gen_funcs import rag_search, sum_search_query
 
 def main():
     parser = argparse.ArgumentParser(description="Retrieval Augmented Generation CLI")
@@ -9,6 +9,12 @@ def main():
         "rag", help="Perform RAG (search + generate answer)"
     )
     rag_parser.add_argument("query", type=str, help="Search query for RAG")
+
+    rag_sum_parser = subparsers.add_parser(
+    "summarize", help="Let the LLM make a summary of a list of movies"
+    )
+    rag_sum_parser.add_argument("query", type=str, help="Search query for Summarize")
+    rag_sum_parser.add_argument("--limit", nargs="?", type=int, default=5, help="Number of search results to retrieve")
 
     args = parser.parse_args()
 
@@ -22,6 +28,15 @@ def main():
             
             print("\nRAG Response:")
             print(f"{llm_response}")
+        case "summarize":
+            sum_search_results, llm_res = sum_search_query(args.query, args.limit)
+            print("Search Results:")
+            for _, data in enumerate(sum_search_results):
+                print(f"    - {data['title']}")
+            
+            print("\nLLM Summary:")
+            print(f"{llm_res}")
+
         case _:
             parser.print_help()
 
